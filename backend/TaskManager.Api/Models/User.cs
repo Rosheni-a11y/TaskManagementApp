0b43a3ed450d327e5cb81285a6fd07e352adfefd
Rosheni-a11y <roshenibolonne@gmail.com>
2026-07-03 15:09:00 +0530

    Initial commit

---
+++ backend/TaskManager.Api/Models/User.cs
@@ -0,0 +1,15 @@
+namespace TaskManager.Api.Models
+{
+    public class User
+    {
+        public int Id {get;set;}
+        public string Name {get;set;} = string.Empty;
+        public string Email {get;set;} = string.Empty;
+        public DateTime CreateDate {get;set;}
+
+
+
+
+
+    }
+}
